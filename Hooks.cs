c32e299c982334427d16acea2dd61b4d66a8d818
SENZO\senzo <senzo.mbuzwa@isometrix.com>
2023-11-10 20:41:28 +0200

    updated

--- Hooks.cs
+++ Hooks.cs
@@ -1,32 +1,91 @@
-﻿using BoDi;
-using AbsaTechAssessment.ReusableMethods;
-using NUnit.Framework;
+﻿using AbsaTechAssessment.ReusableMethods;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
+using AventStack.ExtentReports.Reporter;
 using OpenQA.Selenium;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading;
-using System.Threading.Tasks;
 using TechTalk.SpecFlow;
 
-namespace AbsaTechAssessment
+namespace YourNamespace
 {
-    [Binding]
-    public sealed class Hooks : Validations
+    public class ExtentReportHooks
     {
-        [ThreadStatic]
+        //public static ExtentReports extent;
+        //public static ExtentTest featureName;
+        //public static ExtentTest scenario;
+        public static ExtentHtmlReporter htmlReporter;
 
-        private readonly IObjectContainer _container;
+        static AventStack.ExtentReports.ExtentReports extent;
+        static AventStack.ExtentReports.ExtentTest feature;
+        static AventStack.ExtentReports.ExtentTest scenario;
+        static AventStack.ExtentReports.ExtentTest step;
+        static string reportPath = System.IO.Directory.GetParent(@"../../../").FullName
+            + Path.DirectorySeparatorChar + "Result"
+            + Path.DirectorySeparatorChar + "Result_" + DateTime.Now.ToString("ddMMyyyyyy HHmmss");
 
-        public Hooks(IObjectContainer container)
+        [BeforeTestRun]
+        public static void InitializeReport()
         {
-            _container = container;
+            ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(reportPath);
+            htmlReporter = new ExtentHtmlReporter(reportPath);
+            extent = new AventStack.ExtentReports.ExtentReports();
+            extent.AttachReporter(htmlReporter);
         }
 
-        [AfterScenario]
-        public void AfterScenario()
+        [BeforeFeature]
+        public static void BeforeFeature(FeatureContext context)
         {
+            feature = extent.CreateTest(context.FeatureInfo.Title);
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario(ScenarioContext context)
+        {
+            scenario = feature.CreateNode(context.ScenarioInfo.Title);
+        }
+
+        [BeforeStep]
+        public void BeforeStep(ScenarioContext context)
+        {
+            step = scenario;
+        }
+
+        [AfterStep]
+        public static void InsertReportingSteps()
+        {
+            var stepType = ScenarioStepContext.Current.StepInfo.StepDefinitionType.ToString();
+
+            if (ScenarioContext.Current.TestError == null)
+            {
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "When")
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "Then")
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "And")
+                    scenario.CreateNode<And>(ScenarioStepContext.Current.StepInfo.Text);
+            }
+            else if (ScenarioContext.Current.TestError != null)
+            {
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.InnerException);
+                else if (stepType == "When")
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.InnerException);
+                else if (stepType == "Then")
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.InnerException);
+                else if (stepType == "And")
+                    scenario.CreateNode<And>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.InnerException);
+            }
+        }
+
+        [AfterTestRun]
+        public static void TearDownReport()
+        {
+            extent.Flush();
+            Thread.Sleep(5000);
             Base.driver.Close();
             Base.driver.Quit();
         }
